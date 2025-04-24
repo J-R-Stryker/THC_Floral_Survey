c05a7ae2a050e616502cd4eaa58705ed70c26456
J-R-Stryker <jrs15486@uga.edu>
2025-04-24 12:41:42 -0400

    Adding inital data and code related to floral survey. writing introduction r markdown for basic plotting use for undergraduates

---
+++ Feather_python_code/calibration.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2020 by Bryan Siepert, written for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+import time
+import board
+import busio
+import adafruit_scd30
+
+# SCD-30 has tempremental I2C with clock stretching, datasheet recommends
+# starting at 50KHz
+i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
+scd = adafruit_scd30.SCD30(i2c)
+
+scd.forced_recalibration_reference = 400
